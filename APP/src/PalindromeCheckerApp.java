240d571fa6e3b6393bafff5b1ee99fdc72bfa0fe
kanishk.kshamra05@gmail.com <mj7841@srmist.edu.in>
2026-02-27 10:09:37 +0530

    UC3

--- APP/src/PalindromeCheckerApp.java
+++ APP/src/PalindromeCheckerApp.java
@@ -1,10 +1,17 @@
-
+import java.util.Scanner;
 public class PalindromeCheckerApp {
     static void main() {
-        System.out.println("Welcome to Palindrome Checker Management System");
-        System.out.println("Version:1.0");
-        System.out.println("System initialized successfully.");
-
+        Scanner sc = new Scanner(System.in);
+        String original = sc.nextLine();
+        String reversed = "";
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed += original.charAt(i);
+        }
+        if (original.equalsIgnoreCase(reversed)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not a Palindrome");
+        }
     }
 
 }
