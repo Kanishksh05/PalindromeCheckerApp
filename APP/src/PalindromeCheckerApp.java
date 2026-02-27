9bf289bb70e79a903278bc7c3bee76c929a27cc5
kanishk.kshamra05@gmail.com <mj7841@srmist.edu.in>
2026-02-27 10:46:45 +0530

    UC6

--- APP/src/PalindromeCheckerApp.java
+++ APP/src/PalindromeCheckerApp.java
@@ -1,10 +1,26 @@
-
+import java.util.*;
 public class PalindromeCheckerApp {
     static void main() {
-        System.out.println("Welcome to Palindrome Checker Management System");
-        System.out.println("Version:1.0");
-        System.out.println("System initialized successfully.");
-
-    }
-
-}
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter string: ");
+            String input = sc.nextLine();
+            Stack<Character> stack = new Stack<>();
+            Queue<Character> queue = new LinkedList<>();
+            for (char c : input.toCharArray()) {
+                stack.push(c);
+                queue.add(c);
+            }
+            boolean isPalindrome = true;
+            while (!stack.isEmpty()) {
+                if (!stack.pop().equals(queue.remove())) {
+                    isPalindrome = false;
+                    break;
+                }
+            }
+            if (isPalindrome) {
+                System.out.println("It's a palindrome!");
+            } else {
+                System.out.println("Not a palindrome.");
+            }
+        }
+    };
